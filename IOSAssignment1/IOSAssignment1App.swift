ac5f21b52789c76fc641ef06c3c22e6d1dc47394
NehaBaral <41846320+NehaBaral@users.noreply.github.com>
2024-11-04 14:56:31 -0500

    Initial Commit

---
+++ IOSAssignment1/IOSAssignment1App.swift
@@ -0,0 +1,17 @@
+//
+//  IOSAssignment1App.swift
+//  IOSAssignment1
+//
+//  Created by Neha on 2024-11-04.
+//
+
+import SwiftUI
+
+@main
+struct IOSAssignment1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
